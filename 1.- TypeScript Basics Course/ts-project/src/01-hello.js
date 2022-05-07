e67ac140d3e72050a2c847e25665ded209633405
Aaron Paredes Cabrera <iron0406@outlook.com>
2022-05-07 14:36:24 -0500

    chore: typescript compiler

---
+++ 1.- TypeScript Basics Course/ts-project/src/01-hello.js
@@ -0,0 +1,2 @@
+var myName = '';
+console.log(myName);
---
+++ 1.- TypeScript Basics Course/ts-project/src/01-hello.ts
@@ -0,0 +1,7 @@
+const myName = 'Aaron';
+console.log(myName);
+
+// npx tsc src/01-hello.ts --target es6 --outDir dist
+// The code will be transpiled with target to es6, in the dist folder.
+
+// npx tsc src/*.ts --->>> Transpiles all files
